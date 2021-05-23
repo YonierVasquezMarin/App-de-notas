93dfd001d1a4cb4f63f75e79143d1e7f2ca02d19
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2021-05-23 14:26:31 -0500

    Métodos de Note Handler terminados

--- app/src/main/java/com/midominio/appdenotas/NotesHandler.java
+++ app/src/main/java/com/midominio/appdenotas/NotesHandler.java
@@ -23,10 +23,10 @@ import com.google.gson.reflect.TypeToken;
  */
 public class NotesHandler {
     private ArrayList<Note> arrNotes;
-    private final String fileName = "notes.json";
+    private final String noteLogFileName = "notes.json";
 
     public NotesHandler() {
-
+        arrNotes = this.getNoteListOfTheFile();
     }
 
     //test
@@ -35,9 +35,9 @@ public class NotesHandler {
      * de notas de la targeta SD.
      * @param noteToSave La nota que se guarda en el archivo.
      */
-    public void saveNote(Note noteToSave) {
+    public void saveNotesssss(Note noteToSave) {
         File SDTarget = Environment.getExternalStorageDirectory();
-        File noteFile = new File(SDTarget.getAbsolutePath(), fileName);
+        File noteFile = new File(SDTarget.getAbsolutePath(), noteLogFileName);
 
         String notesContent = "";
 
@@ -78,5 +78,136 @@ public class NotesHandler {
             ex.printStackTrace();
         }
 
+    }
+
+    /**
+     * Busca la nota en el registro de notas del archivo que está guardado en memoría. Si encuentra
+     * la nota ésta se elimina.
+     * @param noteTitleToDelete titulo de la nota a eliminar .
+     * @return retorna true si se encontró y se eliminó la nota. Si la nota no existe se retona false.
+     */
+    private boolean deleteNoteOfTheFile(String noteTitleToDelete) {
+        if (this.arrNotes != null) {
+            Note noteToDelete = this.getNoteOfTheList(noteTitleToDelete);
+            if (noteToDelete!= null) {
+                this.arrNotes.remove(noteToDelete);
+                this.saveNoteListInTheFile(this.arrNotes);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Del archivo de notas que se guarda en memoría se extrae el registro de todas las notas
+     * guardadas, y retorna una lista con todas estas notas
+     * @return Una lista con elementos de la clase Note. Si no hay notas en registradas, se
+     * retorna null.
+     */
+    public ArrayList<Note> getNoteListOfTheFile() {
+        File SDTarget = Environment.getExternalStorageDirectory();
+        File noteFile = new File(SDTarget.getAbsolutePath(), noteLogFileName);
+
+        String notesContent = "";
+
+        //read the file
+        try {
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(noteFile));
+            BufferedReader br = new BufferedReader(isr);
+            String line = br.readLine();
+            while (line!=null) {
+                notesContent += line;
+                line = br.readLine();
+            }
+            br.close();
+            isr.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        if ( !notesContent.isEmpty() ) {
+            Type noteCollectionType = new TypeToken<Collection<Note>>(){}.getType();
+            Gson gson = new Gson();
+            return gson.fromJson(notesContent, noteCollectionType);
+        } else  {
+            return null;
+        }
+    }
+
+    /**
+     * Recibe una lista de notas y la guarda en el archivo que está en memoría.
+     * @param noteListToSave la lista con elementos de la clase Note ha guardar.
+     */
+    private void saveNoteListInTheFile(ArrayList<Note> noteListToSave) {
+        Gson gson = new Gson();
+        String noteListJSONString = gson.toJson(noteListToSave);
+
+        File SDTarget = Environment.getExternalStorageDirectory();
+        File noteFile = new File(SDTarget.getAbsolutePath(), noteLogFileName);
+
+        try {
+            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(noteFile));
+            osw.write(noteListJSONString);
+            osw.flush();
+            osw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Guarda una nota en el registro de notas del archivo que está guardado en memoría.
+     * Si el titulo de la nota ya existe, se modifica el cuerpo de esa nota.
+     * @param noteToSaveInTheFile la nota que se va a guardar en el archivo.
+     */
+    public void saveNoteInTheFile(Note noteToSaveInTheFile) {
+        if (this.arrNotes == null) {
+            arrNotes = new ArrayList<Note>();
+        }
+
+        if (this.arrNotes.contains(noteToSaveInTheFile)) {
+            int indexOfNoteToUpdated = this.arrNotes.indexOf(noteToSaveInTheFile);
+            try {
+                this.arrNotes.add(indexOfNoteToUpdated, noteToSaveInTheFile);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } else {
+            this.arrNotes.add(noteToSaveInTheFile);
+        }
+        this.saveNoteListInTheFile(this.arrNotes);
+    }
+
+    /**
+     * De la lista de notas guardada en esta clase se obtiene la nota y se retorna.
+     * @param titleOfNoteToGet el titulo de la nota a retornar
+     * @return retorna una Note si se encuentra o null si no se encuentra.
+     */
+    public Note getNoteOfTheList(String titleOfNoteToGet) {
+        for(Note noteOfTheList : this.arrNotes) {
+            if (noteOfTheList.getTitle().equals(titleOfNoteToGet)) {
+                return noteOfTheList;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * De la lista de notas de esta clase, se obtiene todos los titulos y se retornan.
+     * @return  una lista con todos los titulos de las notas.
+     */
+    public ArrayList<String> getTitlesOfAllTheNotes() {
+        if(this.arrNotes != null) {
+            ArrayList<String> notesTitlesOfTheList = new ArrayList<String>();
+            for (Note noteOfTheList : this.arrNotes) {
+                notesTitlesOfTheList.add(noteOfTheList.getTitle());
+            }
+            return notesTitlesOfTheList;
+        }
+
+        return null;
     }
 }
